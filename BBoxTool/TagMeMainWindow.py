357b5bf0b7238e9844d63a6606ef55f19148f026
Lucas <lucas@neurala.com>
2017-01-27 17:35:54 -0500

    fixed majority of issues presented yesterday

---
+++ BBoxTool/TagMeMainWindow.py
@@ -0,0 +1,258 @@
+from __future__ import division
+
+from Tkinter import *
+import tkFileDialog as filedialog
+from PIL import Image, ImageTk
+import cv2
+import time
+import os
+import glob
+import csv
+import subprocess
+#import ResizingCanvas
+
+BASE = RAISED
+SELECTED = FLAT
+COLORS = ['red', 'peru', 'DarkOrange1', 'green', 'green3', 'DarkSlateGray4', 'blue', 'cyan', 'orchid1', 'maroon4'] #max of 10 classes for now
+# image sizes for the examples
+SIZE = 256, 256
+
+class mainwindow():
+    def __init__(self, master):
+        self.parent = master
+        self.frame = Toplevel()
+
+        self.frame.columnconfigure(1, weight = 1)
+        self.frame.rowconfigure(14, weight = 1)
+
+        self.tkimg = None
+
+        self.frame.protocol('WM_DELETE_WINDOW', self.doSomething)  # root is your root window
+
+        self.scale = 1.0
+
+        self.orig_img = None
+        self.img_id = None
+
+        #zoom and pan positional data
+        self.xoffset=0.0
+        self.yoffset=0.0
+        self.scaleimg = None
+        self.currentImage = None
+
+        self.hl = None
+        self.vl = None
+        self.bboxId = None
+
+
+        # initialize mouse state
+        self.STATE = {}
+        self.STATE['click'] = 0
+        self.STATE['x'], self.STATE['y'] = 0, 0
+        # ----------------- GUI stuff ---------------------
+
+        # main panel for labeling
+        self.outerFrame = Frame(self.frame)
+        self.outerFrame.pack(fill="both",expand=True)
+
+        self.mainPanel = Canvas(self.outerFrame,width=400, height=400, bd=0, cursor='tcross')
+        self.mainPanel.pack(fill="both", expand=True)
+
+
+        self.ctrPanel = Frame(self.outerFrame)
+        self.ctrPanel.pack(fill="both", expand=True)
+        self.progLabel = Label(self.ctrPanel, text = "Progress:     /    ")
+        self.progLabel.pack(side = LEFT, padx = 5)
+        self.tmpLabel = Label(self.ctrPanel, text = "Go to Image No.")
+        self.tmpLabel.pack(side = LEFT, padx = 5)
+
+
+        self.idxEntry = Entry(self.ctrPanel, width = 5)
+        self.idxEntry.pack(side = LEFT)
+        self.idxEntry.config(bg='white')
+        self.goBtn = Button(self.ctrPanel, text='Go', state=DISABLED, command = self.parent.gotoImage)
+        self.goBtn.pack(side = LEFT)
+        # display mouse position
+        self.disp = Label(self.ctrPanel, text='')
+        self.disp.pack(side = RIGHT)
+        self.initW=0.0
+        self.inith=0.0
+
+        self.splash()
+
+    def resizeWindow(self, event):
+        self.mainPanel.config( width = max(event.width-4,self.initW), height = max(event.height-4,self.inith))
+
+    def splash(self):
+        raw_img = Image.open("./Images/001/splash.jpg") #SPLASH SCREEN SOURCE GOES HERE
+        self.tkimg = ImageTk.PhotoImage(raw_img)
+        self.initW = self.tkimg.width()
+        self.inith = self.tkimg.height()
+        self.mainPanel.config(width=max(self.tkimg.width(), 400), height=max(self.tkimg.height(), 400))
+        self.mainPanel.create_image(0, 0, image=self.tkimg, anchor=NW)
+        self.mainPanel.bind('<Configure>', self.resizeWindow)
+        self.frame.update()
+        self.frame.minsize(width=self.initW, height=self.inith+30)
+
+    def bindInterface(self):
+        self.mainPanel.bind("<Button-1>", self.mouseClick)
+        self.mainPanel.bind("<ButtonRelease-1>", self.mouseRelease)
+        self.mainPanel.bind("<Motion>", self.mouseMove)
+        self.mainPanel.bind("<Escape>", self.cancelBBox)  # press <Escape> to cancel current bbox self.cancelBBox
+        self.mainPanel.bind("<Key>", self.hotkeys)
+        self.mainPanel.config(scrollregion=self.mainPanel.bbox(ALL))
+
+        self.mainPanel.bind("<Button-4>", self.zoom)
+        self.mainPanel.bind("<Button-5>", self.zoom)
+
+        self.mainPanel.bind("<Left>", self.parent.prevImage)
+        self.mainPanel.bind("<Right>", self.parent.nextImage)
+        self.mainPanel.bind("<MouseWheel>", self.zoom)
+        self.goBtn.config(state=NORMAL)
+
+    def mouseRelease(self,event):
+        if self.STATE['click'] == 1:
+            x1, x2 = min(self.STATE['x'], event.x), max(self.STATE['x'], event.x)
+            y1, y2 = min(self.STATE['y'], event.y), max(self.STATE['y'], event.y)
+
+            self.mainPanel.itemconfig(self.bboxId, dash=(2,2))
+            self.parent.bboxIdList.append(self.bboxId)
+            width= x2-x1
+            height= y2-y1
+            self.parent.bboxList.append((self.parent.currentLabel, float((x1+(width/2.0)) / self.tkimg.width()), float((y1+(height/2.0)) / self.tkimg.height()),
+                                  float(width / self.tkimg.width()), float(height / self.tkimg.height())))
+
+            self.bboxId = None
+            self.parent.listbox.insert(END, '(%d, %d) -> (%d, %d)' % (x1, y1, x2, y2))
+            self.parent.listbox.itemconfig(len(self.parent.bboxIdList) - 1, fg=COLORS[self.parent.currentLabel])
+            self.STATE['click']=0
+
+    def mouseClick(self, event):
+        if self.STATE['click'] == 0:
+            self.STATE['x'], self.STATE['y'] = event.x, event.y
+            self.STATE['click'] =1
+
+    def mouseMove(self, event):
+        self.disp.config(text = 'x: %d, y: %d' %(event.x, event.y))
+        self.mainPanel.focus_set()
+        if self.tkimg:
+            if self.hl:
+                self.mainPanel.delete(self.hl)
+            self.hl = self.mainPanel.create_line(0, event.y, self.tkimg.width(), event.y, width = 2)
+            if self.vl:
+                self.mainPanel.delete(self.vl)
+            self.vl = self.mainPanel.create_line(event.x, 0, event.x, self.tkimg.height(), width = 2)
+        if 1 == self.STATE['click']:
+            if self.bboxId:
+                self.mainPanel.delete(self.bboxId)
+            self.bboxId = self.mainPanel.create_rectangle(self.STATE['x'], self.STATE['y'], \
+                                                            event.x, event.y, \
+                                                            width = 2, \
+                                                            outline = COLORS[self.parent.currentLabel],dash=(2, 4))
+    def cancelBBox(self, event):
+        if 1 == self.STATE['click']:
+            if self.bboxId:
+                self.mainPanel.delete(self.bboxId)
+                self.bboxId = None
+                self.STATE['click'] = 0
+
+    def hotkeys(self, event):
+
+        if event.char == "1":
+            self.parent.activelabels(1)
+        elif event.char == "2":
+            self.parent.activelabels(2)
+        elif event.char == "3":
+            self.parent.activelabels(3)
+        elif event.char == "4":
+            self.parent.activelabels(4)
+        elif event.char == "5":
+            self.parent.activelabels(5)
+        elif event.char == "6":
+            self.parent.activelabels(6)
+        elif event.char == "7":
+            self.parent.activelabels(7)
+        elif event.char == "8":
+            self.parent.activelabels(8)
+        elif event.char == "9":
+            self.parent.activelabels(9)
+        elif event.char == "0":
+            self.parent.activelabels(10)
+        elif event.char == "c":
+            self.clearBBox()
+        elif event.char == "a":
+            self.parent.prevImage()
+        elif event.char == "d":
+            self.parent.nextImage()
+        elif event.char == "z":
+            self.parent.delBBox(True)
+        elif event.char == 's':
+            self.parent.saveImage()
+        self.parent.activelabel.config(text='Active Label: '+str(self.parent.currentLabel+1),bg=COLORS[self.parent.currentLabel])
+    def zoom(self, event):
+
+        self.mainPanel.move(ALL, -self.xoffset, -self.yoffset)
+        if event.num == 4 or event.delta == 120:
+            self.scale *= 1.1
+            self.mainPanel.scale(ALL, 0, 0, 1.1, 1.1)
+        if event.num == 5 or event.delta == -120:
+            self.scale *= 0.9
+            self.mainPanel.scale(ALL, 0, 0, 0.9, 0.9)
+
+        self.redraw()
+
+    def redraw(self, x=0, y=0):
+        if self.img_id:
+            self.mainPanel.delete(self.img_id)
+        iw, ih = self.orig_img.size
+        print iw
+        print ih
+        print self.scale
+        size = int(iw * self.scale), int(ih * self.scale)
+        print size
+        self.tkimg = ImageTk.PhotoImage(image=self.orig_img.resize(size))
+
+        self.img_id = self.mainPanel.create_image(x, y, image=self.tkimg, anchor=NW)
+        self.mainPanel.tag_lower(self.img_id)
+        self.mainPanel.move(ALL, self.xoffset, self.yoffset)
+
+    def grab(self, event):
+        self._y = event.y
+        self._x = event.x
+
+    def drag(self, event):
+
+        if (self._y - event.y < 0):
+            self.yoffset += 10
+            self.mainPanel.move(ALL, 0, 10)
+        elif (self._y - event.y > 0):
+            self.yoffset -= 10
+            self.mainPanel.move(ALL, 0, -10)
+        if (self._x - event.x < 0):
+            self.xoffset += 10
+            self.mainPanel.move(ALL, 10, 0)
+        elif (self._x - event.x > 0):
+            self.xoffset -= 10
+            self.mainPanel.move(ALL, -10, 0)
+
+        if self.img_id:
+            self.mainPanel.delete(self.img_id)
+
+        self.img_id = self.mainPanel.create_image(self.xoffset, self.yoffset, image=self.img, anchor=NW)
+        self.mainPanel.tag_lower(self.img_id)
+
+        self._x = event.x
+        self._y = event.y
+
+    def loadImage(self,imagepath):
+        self.img = Image.open(imagepath)
+        self.tkimg = ImageTk.PhotoImage(self.img)
+        self.orig_img = self.img
+        #.mainPanel.config(width=max(self.tkimg.width(), 400), height = max(self.tkimg.height(), 400))
+        self.mainPanel.create_image(0, 0, image=self.tkimg, anchor=NW)
+        self.progLabel.config(text="%04d/%04d" % (self.parent.cur, self.parent.total))
+        self.parent.clearBBox()
+
+
+    def doSomething(self):
+        print "You can't do that"
--- BBoxTool/TagMeRC2.py
+++ BBoxTool/TagMeRC2.py
@@ -18,6 +18,7 @@ import os
 import glob
 import csv
 import subprocess
+from TagMeMainWindow import mainwindow
 
 BASE = RAISED
 SELECTED = FLAT
@@ -34,8 +35,8 @@ class LabelTool():
         self.parent = master
         self.parent.title("Neurala TagMeTool")
         self.frame = Frame(self.parent)
-        self.frame.pack(fill=BOTH, expand=True)
-        self.parent.resizable(width = True, height = True)
+        self.parent.resizable(width=False, height=False)
+        self.frame.pack(fill=BOTH, expand=False)
 
         # initialize global state
         self.imageDir = ''
@@ -48,35 +49,24 @@ class LabelTool():
         self.category = 0
         self.imagename = ''
         self.labelfilename = ''
-        self.tkimg = None
+
         self.classlist = []
         self.classnames= ["" for x in range(10)]
         self.classbuttons = []
-        # initialize mouse state
-        self.STATE = {}
-        self.STATE['click'] = 0
-        self.STATE['x'], self.STATE['y'] = 0, 0
+
+
+        self.currentLabel = 0
 
         # reference to bbox
         self.bboxIdList = []
-        self.bboxId = None
+
         self.bboxList = []
-        self.hl = None
-        self.vl = None
+
         self.tree = None
 
 
-        self.xoffset=0.0
-        self.yoffset=0.0
-        self.scaleimg = None
-        self.currentImage = None
-        self.currentLabel = 0
-
-
-        self.scale = 1.0
-        self.orig_img = None
         self.img = None
-        self.img_id = None
+
 
         #popup windows
         self.top = None
@@ -84,73 +74,54 @@ class LabelTool():
 
         # ----------------- GUI stuff ---------------------
 
-        # main panel for labeling
-        self.mainPanel = Canvas(self.frame, bd=0, cursor='tcross')
-
-        self.mainPanel.grid(row=1, column=1, rowspan=13, sticky=N+W)
-
         # showing bbox info & delete bbox
         self.lb1 = Label(self.frame, text = 'Bounding boxes:')
-        self.lb1.grid(row = 1, column = 2,  sticky = W+N)
+        self.lb1.grid(row = 1, column = 1,  sticky = W+N)
         self.listbox = Listbox(self.frame, width = 24, height = 12)
-        self.listbox.grid(row = 2, column = 2, sticky = N)
+        self.listbox.grid(row = 2, column = 1, sticky = N)
         self.activelabel = Label(self.frame, text='Active Label: '+str(self.currentLabel+1),bg=COLORS[self.currentLabel])
-        self.activelabel.grid(row=3,column=2,sticky=W+E+N)
+        self.activelabel.grid(row=3,column=1,sticky=W+E+N)
         #help button
         self.help = Button(self.frame, text='Help', command= self.showhelp)
-        self.help.grid(row=4, column=2, sticky=W+E+N)
+        self.help.grid(row=4, column=1, sticky=W+E+N)
         # dir entry & load
         self.ldBtn = Button(self.frame, text="Load Directory",bg='peru', command=self.loadDir)  # command
-        self.ldBtn.grid(row=5, column=2, sticky=W+E+N)
+        self.ldBtn.grid(row=5, column=1, sticky=W+E+N)
 
         #bbox label and save
         self.labelentry = Frame(self.frame)
-        self.labelentry.grid(row=6, column=2, sticky=W+E+N)
+        self.labelentry.grid(row=6, column=1, sticky=W+E+N)
         self.labelsave = None
         self.classdefine()
 
 
         self.undo = Button(self.frame, text= 'Undo Label',state=DISABLED, command = lambda: self.delBBox(True))
-        self.undo.grid(row=7, column=2, sticky=W+E+N+S)
+        self.undo.grid(row=7, column=1, sticky=W+E+N+S)
 
         self.btnDel = Button(self.frame, text = 'Delete Selected',state=DISABLED, command = self.delBBox)
-        self.btnDel.grid(row = 8, column = 2, sticky = W+E+S+N)
+        self.btnDel.grid(row = 8, column = 1, sticky = W+E+S+N)
         self.btnClear = Button(self.frame, text = 'Clear All',state=DISABLED, command = self.clearBBox)
-        self.btnClear.grid(row = 9, column = 2, sticky = W+E+S+N)
+        self.btnClear.grid(row = 9, column = 1, sticky = W+E+S+N)
 
         # control panel for image navigation
-        self.ctrPanel = Frame(self.frame)
-        self.ctrPanel.grid(row = 10, column = 1, columnspan = 2, sticky = W+E)
-        self.prevBtn = Button(self.ctrPanel, text='<< Prev', state=DISABLED, width = 10, command = self.prevImage)
-        self.prevBtn.pack(side = LEFT, padx = 5, pady = 3)
-        self.nextBtn = Button(self.ctrPanel, text='Next >>',state=DISABLED, width = 10, command = self.nextImage)
-        self.nextBtn.pack(side = LEFT, padx = 5, pady = 3)
-        self.progLabel = Label(self.ctrPanel, text = "Progress:     /    ")
-        self.progLabel.pack(side = LEFT, padx = 5)
-        self.tmpLabel = Label(self.ctrPanel, text = "Go to Image No.")
-        self.tmpLabel.pack(side = LEFT, padx = 5)
-        self.idxEntry = Entry(self.ctrPanel, width = 5)
-        self.idxEntry.pack(side = LEFT)
-        self.goBtn = Button(self.ctrPanel, text='Go', state=DISABLED, command = self.gotoImage)
-        self.goBtn.pack(side = LEFT)
 
-        self.mainPanel.focus_set()
-        # display mouse position
-        self.disp = Label(self.ctrPanel, text='')
-        self.disp.pack(side = RIGHT)
+        self.navpanel = Frame(self.frame)
+        self.navpanel.grid(row = 11, column = 1, columnspan = 2, sticky = W+E)
+        self.prevBtn = Button(self.navpanel, text='<< Prev', state=DISABLED, command = self.prevImage)
+        self.prevBtn.grid(row = 1, column = 1, sticky = W+E)
+        self.nextBtn = Button(self.navpanel, text='Next >>',state=DISABLED, command = self.nextImage)
+        self.nextBtn.grid(row = 1, column = 2, sticky = W+E)
 
-        self.frame.columnconfigure(1, weight = 1)
-        self.frame.rowconfigure(4, weight = 1)
 
-        self.splash()
+        # main panel for labeling
+        self.mainPanel = mainwindow(self)
+
+
         self.center(self.parent)
+        self.center(self.mainPanel.frame)
         self.showhelp()
 
-    def splash(self):
-        raw_img = Image.open("./Images/001/splash.jpg") #SPLASH SCREEN SOURCE GOES HERE
-        self.tkimg = ImageTk.PhotoImage(raw_img)
-        self.mainPanel.config(width=max(self.tkimg.width(), 400), height=max(self.tkimg.height(), 400))
-        self.mainPanel.create_image(0, 0, image=self.tkimg, anchor=NW)
+
 
     def center(self,toplevel):
         toplevel.update_idletasks()
@@ -202,39 +173,7 @@ class LabelTool():
             self.currentLabel = 9
         self.activelabel.config(text='Active Label: ' + str(self.currentLabel + 1), bg=COLORS[self.currentLabel])
 
-    def hotkeys(self, event):
 
-        if event.char == "1":
-            self.activelabels(1)
-        elif event.char == "2":
-            self.activelabels(2)
-        elif event.char == "3":
-            self.activelabels(3)
-        elif event.char == "4":
-            self.activelabels(4)
-        elif event.char == "5":
-            self.activelabels(5)
-        elif event.char == "6":
-            self.activelabels(6)
-        elif event.char == "7":
-            self.activelabels(7)
-        elif event.char == "8":
-            self.activelabels(8)
-        elif event.char == "9":
-            self.activelabels(9)
-        elif event.char == "0":
-            self.activelabels(10)
-        elif event.char == "c":
-            self.clearBBox()
-        elif event.char == "a":
-            self.prevImage()
-        elif event.char == "d":
-            self.nextImage()
-        elif event.char == "z":
-            self.delBBox(True)
-        elif event.char == 's':
-            self.saveImage()
-        self.activelabel.config(text='Active Label: '+str(self.currentLabel+1),bg=COLORS[self.currentLabel])
 
     def loadDir(self, dbg=False):
         #open dialog box to select data, allow only image files
@@ -299,37 +238,33 @@ class LabelTool():
             with open(self.labelfilename,'r') as f:
                 reader = csv.reader(f, delimiter=" ")
                 for row in reader:
-                    print str(self.tkimg.width())
+                    print str(self.mainPanel.tkimg.width())
                     self.bboxList.append(row)
-                    x1 = int((float(row[1])-(float(row[3])/2.0)) * self.tkimg.width())
-                    y1 = int((float(row[2])-(float(row[4])/2.0)) * self.tkimg.height())
-                    x2 = int((float(row[1])+(float(row[3])/2.0)) * self.tkimg.width())
-                    y2 = int((float(row[2])+(float(row[4])/2.0)) * self.tkimg.height())
-                    tmpId = self.mainPanel.create_rectangle(x1, y1, x2, y2, width=2, outline=COLORS[int(row[0])])
+
+                    x1 = int((float(row[1])-(float(row[3])/2.0)) * self.mainPanel.tkimg.width())
+                    y1 = int((float(row[2])-(float(row[4])/2.0)) * self.mainPanel.tkimg.height())
+                    x2 = int((float(row[1])+(float(row[3])/2.0)) * self.mainPanel.tkimg.width())
+                    y2 = int((float(row[2])+(float(row[4])/2.0)) * self.mainPanel.tkimg.height())
+
+                    tmpId = self.mainPanel.mainPanel.create_rectangle(x1, y1, x2, y2, width=2, outline=COLORS[int(row[0])])
                     self.bboxIdList.append(tmpId)
                     self.listbox.insert(END, '(%f, %f) -> (%f, %f)' % (float(row[1]), float(row[2]), float(row[3]), float(row[4])))
                     self.listbox.itemconfig(len(self.bboxIdList) - 1,
                                             fg=COLORS[int(row[0])])
-                self.mainPanel.scale(ALL, 0, 0, self.scale, self.scale)
+                self.mainPanel.mainPanel.scale(ALL, 0, 0, self.mainPanel.scale, self.mainPanel.scale)
 
     def loadImage(self):
         # load image
         imagepath = self.imageList[self.cur - 1]
 
-        self.img = Image.open(imagepath)
-        self.tkimg = ImageTk.PhotoImage(self.img)
-        self.orig_img = self.img
-        self.mainPanel.config(width=max(self.tkimg.width(), 400), height = max(self.tkimg.height(), 400))
-        self.mainPanel.create_image(0, 0, image=self.tkimg, anchor=NW)
-        self.progLabel.config(text="%04d/%04d" % (self.cur, self.total))
+        self.mainPanel.loadImage(imagepath)
 
-        self.clearBBox()
         self.imagename = os.path.split(imagepath)[-1].split('.')[0]
         labelname = self.imagename + '.txt'
         self.labelfilename = os.path.join(self.outDir, labelname)
         if os.path.exists(self.labelfilename):
             self.readfile()
-        self.redraw()
+        self.mainPanel.redraw()
 
     def saveImage(self):
         with open(self.labelfilename, 'w') as f:
@@ -342,105 +277,6 @@ class LabelTool():
             f.close()
         print 'Image No. %d saved to %s' % (self.cur, self.labelfilename)
 
-    def grab(self, event):
-        self._y = event.y
-        self._x = event.x
-
-    def drag(self, event):
-        if (self._y - event.y < 0):
-            self.yoffset += 10
-            self.mainPanel.move(ALL, 0, 10)
-        elif (self._y - event.y > 0):
-            self.yoffset -= 10
-            self.mainPanel.move(ALL, 0, -10)
-        if (self._x - event.x < 0):
-            self.xoffset += 10
-            self.mainPanel.move(ALL, 10, 0)
-        elif (self._x - event.x > 0):
-            self.xoffset -= 10
-            self.mainPanel.move(ALL, -10, 0)
-
-        if self.img_id:
-            self.mainPanel.delete(self.img_id)
-
-        self.img_id = self.mainPanel.create_image(self.xoffset, self.yoffset, image=self.img, anchor=NW)
-        self.mainPanel.tag_lower(self.img_id)
-
-        self._x = event.x
-        self._y = event.y
-
-    def zoom(self, event):
-
-        self.mainPanel.move(ALL, -self.xoffset, -self.yoffset)
-        if event.num == 4 or event.delta == 120:
-            self.scale *= 1.1
-            self.mainPanel.scale(ALL, 0, 0, 1.1, 1.1)
-        if event.num == 5 or event.delta == -120:
-            self.scale *= 0.9
-            self.mainPanel.scale(ALL, 0, 0, 0.9, 0.9)
-
-        self.redraw()
-
-    def redraw(self, x=0, y=0):
-        if self.img_id:
-            self.mainPanel.delete(self.img_id)
-        iw, ih = self.orig_img.size
-        print iw
-        print ih
-        print self.scale
-        size = int(iw * self.scale), int(ih * self.scale)
-        print size
-        self.tkimg = ImageTk.PhotoImage(image=self.orig_img.resize(size))
-
-        self.img_id = self.mainPanel.create_image(x, y, image=self.tkimg, anchor=NW)
-        self.mainPanel.tag_lower(self.img_id)
-        self.mainPanel.move(ALL, self.xoffset, self.yoffset)
-
-    def mouseRelease(self,event):
-        x1, x2 = min(self.STATE['x'], event.x), max(self.STATE['x'], event.x)
-        y1, y2 = min(self.STATE['y'], event.y), max(self.STATE['y'], event.y)
-
-        self.mainPanel.itemconfig(self.bboxId, dash=(2,2))
-        self.bboxIdList.append(self.bboxId)
-        width= x2-x1
-        height= y2-y1
-        self.bboxList.append((self.currentLabel, float((x1+(width/2.0)) / self.tkimg.width()), float((y1+(height/2.0)) / self.tkimg.height()),
-                              float(width / self.tkimg.width()), float(height / self.tkimg.height())))
-
-        self.bboxId = None
-        self.listbox.insert(END, '(%d, %d) -> (%d, %d)' % (x1, y1, x2, y2))
-        self.listbox.itemconfig(len(self.bboxIdList) - 1, fg=COLORS[self.currentLabel])
-        self.STATE['click']=0
-
-    def mouseClick(self, event):
-        if self.STATE['click'] == 0:
-            self.STATE['x'], self.STATE['y'] = event.x, event.y
-            self.STATE['click'] =1
-
-    def mouseMove(self, event):
-        self.disp.config(text = 'x: %d, y: %d' %(event.x, event.y))
-        self.mainPanel.focus_set()
-        if self.tkimg:
-            if self.hl:
-                self.mainPanel.delete(self.hl)
-            self.hl = self.mainPanel.create_line(0, event.y, self.tkimg.width(), event.y, width = 2)
-            if self.vl:
-                self.mainPanel.delete(self.vl)
-            self.vl = self.mainPanel.create_line(event.x, 0, event.x, self.tkimg.height(), width = 2)
-        if 1 == self.STATE['click']:
-            if self.bboxId:
-                self.mainPanel.delete(self.bboxId)
-            self.bboxId = self.mainPanel.create_rectangle(self.STATE['x'], self.STATE['y'], \
-                                                            event.x, event.y, \
-                                                            width = 2, \
-                                                            outline = COLORS[self.currentLabel],dash=(2, 4))
-
-    def cancelBBox(self, event):
-        if 1 == self.STATE['click']:
-            if self.bboxId:
-                self.mainPanel.delete(self.bboxId)
-                self.bboxId = None
-                self.STATE['click'] = 0
 
     def delBBox(self,last = False):
         if last:
@@ -454,14 +290,14 @@ class LabelTool():
                 return
             idx = int(sel[0])
 
-        self.mainPanel.delete(self.bboxIdList[idx])
+        self.mainPanel.mainPanel.delete(self.bboxIdList[idx])
         self.bboxIdList.pop(idx)
         self.bboxList.pop(idx)
         self.listbox.delete(idx)
 
     def clearBBox(self):
         for idx in range(len(self.bboxIdList)):
-            self.mainPanel.delete(self.bboxIdList[idx])
+            self.mainPanel.mainPanel.delete(self.bboxIdList[idx])
         self.listbox.delete(0, len(self.bboxList))
         self.bboxIdList = []
         self.bboxList = []
@@ -522,27 +358,16 @@ class LabelTool():
         print "saved labels! " +labelfile
 
     def bindCanvasTools(self):
-        self.mainPanel.bind("<Button-1>", self.mouseClick)
-        self.mainPanel.bind("<ButtonRelease-1>", self.mouseRelease)
-        self.mainPanel.bind("<Motion>", self.mouseMove)
-        self.parent.bind("<Escape>", self.cancelBBox)  # press <Escape> to cancel current bbox self.cancelBBox
-        self.mainPanel.bind("<Key>", self.hotkeys)
-        self.mainPanel.config(scrollregion=self.mainPanel.bbox(ALL))
 
-        self.mainPanel.bind("<Button-4>", self.zoom)
-        self.mainPanel.bind("<Button-5>", self.zoom)
+        self.mainPanel.bindInterface()
 
-        self.parent.bind("<Left>", self.prevImage)
-        self.parent.bind("<Right>", self.nextImage)
-
-        self.mainPanel.bind("<MouseWheel>", self.zoom)
         self.undo.config(state=NORMAL)
         self.btnDel.config(state=NORMAL)
         self.btnClear.config(state=NORMAL)
         self.prevBtn.config(state=NORMAL)
         self.nextBtn.config(state=NORMAL)
-        self.goBtn.config(state=NORMAL)
-        self.idxEntry.config(bg='white')
+
+
 
 if __name__ == '__main__':
     root = Tk()
